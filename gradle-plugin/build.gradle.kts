396933fb2baa88359326e49e865684aa627c6e49
TBSten <81161390+TBSten@users.noreply.github.com>
2024-11-19 21:42:50 +0900

    公開用設定を追加

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinJvm)
     `java-gradle-plugin`
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 dependencies {
@@ -14,10 +17,49 @@ gradlePlugin {
     plugins {
         create("ktor-static-generation") {
             id = "me.tbsten.ktor.static.generation"
-            version = "0.1.0-dev04"
             implementationClass =
                 "me.tbsten.ktor.staticGeneration.KtorStaticGenerationPlugin"
             displayName = "Ktor static generation plugin"
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    signAllPublications()
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "ktor-static-generation-gradle-plugin",
+        version = version.toString(),
+    )
+
+    val libraryGithubUrl = "https://github.com/tbsten/ktor-static-generation"
+
+    pom {
+        name.set("Ktor Static Generation Gradle Plugin")
+        description.set("ktor plugin to output routes statically.")
+        inceptionYear.set("2024")
+        url.set(libraryGithubUrl)
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("tbsten")
+                name.set("TBSten")
+                url.set("https://github.com/tbsten/")
+            }
+        }
+        scm {
+            url.set(libraryGithubUrl)
+            connection.set("scm:git:git://github.com/TBSten/ktor-static-generation.git")
+            developerConnection.set("scm:git:ssh://git@github.com/TBSten/ktor-static-generation.git")
         }
     }
 }
--- ktor-static-generation/build.gradle.kts
+++ ktor-static-generation/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 kotlin {
@@ -27,6 +30,46 @@ kotlin {
             dependencies {
                 implementation(libs.ktor.server.test.host)
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+
+    signAllPublications()
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "ktor-static-generation",
+        version = version.toString(),
+    )
+
+    val libraryGithubUrl = "https://github.com/tbsten/ktor-static-generation"
+
+    pom {
+        name.set("Ktor Static Generation Plugin")
+        description.set("ktor plugin to output routes statically.")
+        inceptionYear.set("2024")
+        url.set(libraryGithubUrl)
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("tbsten")
+                name.set("TBSten")
+                url.set("https://github.com/tbsten/")
+            }
+        }
+        scm {
+            url.set(libraryGithubUrl)
+            connection.set("scm:git:git://github.com/TBSten/ktor-static-generation.git")
+            developerConnection.set("scm:git:ssh://git@github.com/TBSten/ktor-static-generation.git")
         }
     }
 }
