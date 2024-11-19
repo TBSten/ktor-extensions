92146eb13918424f02300091da5dd6d57e833090
TBSten <81161390+TBSten@users.noreply.github.com>
2024-11-19 21:44:33 +0900

    jvm プロジェクトに変更

--- ktor-static-generation/build.gradle.kts
+++ ktor-static-generation/build.gradle.kts
@@ -1,37 +1,17 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
-kotlin {
-    jvm()
+kotlin {}
 
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
-        val jvmMain by getting {
-            dependencies {
-                implementation(libs.ktor.server.core)
-                implementation(libs.ktor.server.resources)
-                implementation(libs.ktor.server.test.host)
-                implementation(libs.ktor.server.netty)
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(libs.ktor.server.test.host)
-            }
-        }
-    }
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.resources)
+    implementation(libs.ktor.server.test.host)
+    implementation(libs.ktor.server.netty)
 }
 
 mavenPublishing {
