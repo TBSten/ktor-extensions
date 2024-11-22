fffadc8c7836afa939c234f26b419c67a8613db9
TBSten <81161390+TBSten@users.noreply.github.com>
2024-11-23 06:27:58 +0900

    gradle plugin のリファクタ

--- gradle-plugin/src/main/kotlin/me/tbsten/ktor/staticGeneration/KtorStaticGenerationTask.kt
+++ gradle-plugin/src/main/kotlin/me/tbsten/ktor/staticGeneration/KtorStaticGenerationTask.kt
@@ -5,22 +5,14 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.JavaExec
 import javax.inject.Inject
 
-/**
- *
- * ```kt
- * staticGeneration { // this: JavaExec
- *     mainClass.set("me.tbsten.ktor.staticGeneration.test.sg.StaticGenerationKt")
- *     classpath.from(sourceSets.main) // TODO 適用されているpluginをみて自動でデフォルト値を設定する
- *     /* outputDir = layout.buildDirectory.dir("ktor-static-generate-output") */
- * }
- * ```
- */
 open class KtorStaticGenerationTask
     @Inject
-    constructor(project: Project) : JavaExec() {
+    constructor(
+        project: Project,
+    ) : JavaExec() {
         init {
             // TODO projectに適用されているpluginに応じてclasspathを設定
-//        classpath.from(sourceSets.main)
+//        classpath(sourceSets.main.get().runtimeClasspath)
         }
 
         @Input
@@ -28,5 +20,6 @@ open class KtorStaticGenerationTask
             project
                 .layout.buildDirectory
                 .dir("ktor-static-generate-output")
-                .get().asFile.absolutePath
+                .get()
+                .asFile.absolutePath
     }
