3c1d65e1a48ea02d77dd6ba0d9ddad65e1710f11
TBSten <81161390+TBSten@users.noreply.github.com>
2024-11-23 07:47:51 +0900

    TYPESAFE_PROJECT_ACCESSORS を有効化

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,8 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ktor-static-generation"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "ktor-static-generation-root"
 include(":gradle-plugin")
 include(":ktor-static-generation")
